64e0b18e7bbf6a76ee6b65381749d531d8cc73aa
SondosAbuYounis <abuyounissundos@gmail.com>
2023-10-10 01:13:55 +0300

    cards are done

--- app.js
+++ app.js
@@ -1,43 +1,75 @@
 // i created a constructor to generate product objects cards 
-function productCardObj (Title, Price, Image){
-    this.Title 
-    this.Price
-    this.Image
-}
+// function productCardObj (Title, Price, Image){
+//     this.Title 
+//     this.Price
+//     this.Image
+// }
 // i created a constructor to push product info in it   
-const productObj = [];
+var productArr = [];
+// const productObject = {}
+
 // read the data in the API 
-fetch('https://fakestoreapi.com/products')
+
+fetch('https://fakestoreapi.com/products?limit=20')
 .then(Response=>{
     if (!Response.ok){
         throw new Error('Network response was not ok');
     }
     return Response.json()
 })
-.then(json=>console.log(json))
+.then(json=> {
+    console.log("txt ", json)
+    return json 
+})
 // pushed the data into a new array
 .then(data => {
-    productObj.push({
-    Title: data.Title,
-    Price: data.Price,
-    Image: data.Image,
-})
+    console.log("data ", data)
+    productArr = data
+    localStorage.setItem("productarray", JSON.stringify(productArr))
+    // console.log("getLocal", )
+    var localStorageData = JSON.parse(localStorage.getItem("productarray"));
+    cardDisplay(localStorageData)
+    cardDisplay(productArr)
+
 })
 .catch(error => {
     console.error('There was a problem with the fetch operation:', error)
 })
 
 // objects which will be rendered in the main section as a card from your app.js file.
+function cardDisplay (cardsInfo) {
+    console.log("info ", cardsInfo)
+// const productObjCard = productObj.map (obj => {
+//     for (key in objJsonRet) {
+// var productObjArray = JSON.parse(productArr);
 
-const productObjCard = productObj.map (obj => {
-    for (key in objJsonRet) {
-        const cardData = `
-            <div class="prodCard">
-                <div>${Title}</div>
-                <div>${Price}</div>
-                <div>${Image}</div>
+const productCardCo = document.getElementById("productCard");
+    cardsInfo.map((product)=>{
+        var cardData = `
+        <div class="prodCard" >
+            <img src="${product.image}" alt="${product.title}">
+            <div class="cardContents">
+            <p><span>Title:</span> ${product.title}</p>
+            <p><span>Price:</span> ${product.price}</p>
+            <p><span>Category:</span> ${product.category}</p>
             </div>
-            `;
-            document.getElementById('productObjCard').innerHTML += cardData
+        </div>
+        `;
+        // document.getElementById('productObjCard').innerHTML += cardData;
+        productCardCo.innerHTML += cardData;
+    })
+        
     }
-})
+// }
+// })
+
+
+
+
+
+// local storage 
+        // localStorage.setItem("productarray", JSON.stringify(productArr))
+        // // console.log("getLocal", )
+        // var localStorageData = JSON.parse(localStorage.getItem("productarray"));
+        // cardDisplay(localStorageData)
+
